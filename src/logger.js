0174c6d392ef3f232db99e792352048a2a98ee99
bellodigital <abuafradigital@gmail.com>
2026-05-13 09:28:06 +0100

    Update logger.js

--- src/logger.js
+++ src/logger.js
@@ -1,1 +1,28 @@
 
+const winston = require('winston');
+const config = require('../config');
+const fs = require('fs');
+
+if (!fs.existsSync('logs')) fs.mkdirSync('logs');
+
+const logger = winston.createLogger({
+  level: config.logLevel,
+  format: winston.format.combine(
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    winston.format.printf(({ timestamp, level, message }) => {
+      return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
+    })
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: 'logs/bot.log',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+      ),
+    }),
+  ],
+});
+
+module.exports = logger;
